9beb6f4ec9ac7affed8deea5692a77ed93e38ff3
Samuel Jones <samjones714@gmail.com>
2020-02-29 17:23:25 +0000

    Add login activity and connection via firebase

--- Client/app/src/main/java/uk/ac/aber/dcs/mmp/faa/ui/main/MainActivity.kt
+++ Client/app/src/main/java/uk/ac/aber/dcs/mmp/faa/ui/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package uk.ac.aber.dcs.mmp.faa.ui.main
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,9 @@ import androidx.navigation.NavDestination
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import uk.ac.aber.dcs.mmp.faa.R
 
@@ -16,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private val startDestinations = setOf(R.id.homeFragment, R.id.savedFragment, R.id.findCatFragment)
-
+    private val RC_SIGN_IN = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -96,7 +101,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun doLogin() {
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
 
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                println(user.toString())
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
 
